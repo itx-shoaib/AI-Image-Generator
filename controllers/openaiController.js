d3644f22fb9aa39924f2c63b7570a4df2741cd5b
itx-shoaib <shoaibjamil43@gmail.com>
2022-12-19 02:19:20 +0500

    Backend work done

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,11 +6,16 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateimage = async (req, res) => {
+    const { prompt, size } = req.body
+
+    const imageSize = size === "small" ? '256x256' : size === "medium" ? '512x512' : '1024x1024'
+
+
     try {
         const response = await openai.createImage({
-            prompt: "Polar Bear on ice",
+            prompt,
             n: 1,
-            size: "512x512"
+            size: imageSize
         });
 
         const imageurl = response.data.data[0].url
